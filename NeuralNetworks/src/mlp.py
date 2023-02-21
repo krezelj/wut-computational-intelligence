0ffd65cdb30af1aca369f84ad990734c752d4362
Jasiek <jasiekk9@gmail.com>
2023-02-21 12:23:28 +0100

    Implement Feed Forward

--- NeuralNetworks/src/mlp.py
+++ NeuralNetworks/src/mlp.py
@@ -6,10 +6,10 @@ class MLP():
 
     __slots__ = ['layers']
 
-    def __init__(self, steps):
-        pass
+    def __init__(self, layers):
+        self.layers = layers
 
-    def forward(self, input):
+    def predict(self, input):
         output = input
         for layer in self.layers:
             output = layer.forward(output)
@@ -24,24 +24,25 @@ class Layer():
     def __init__(self, input_dim, output_dim, weights=None, biases=None, activation="tanh"):
         self.input_dim = input_dim
         self.output_dim = output_dim
+        self.activation = activation
 
         if weights is None:
             self.__init_weights()
         else:
-            assert(weights.shape == (input_dim, output_dim))
+            assert(weights.shape == (output_dim, input_dim))
             self.weights = weights
 
         if biases is None:
-            assert(biases.shape == (output_dim))
             self.__init_biases()
         else:
+            assert(biases.shape == (output_dim,))
             self.biases = biases
 
     def forward(self, input):
         output = self.weights @ input + self.biases
-        return self.__activation(output)
+        return self.__activate(output)
 
-    def __activation(self, values):
+    def __activate(self, values):
         if self.activation == "tanh":
             return np.tanh(values)
         elif self.activation == "sigmoid":
@@ -55,7 +56,20 @@ class Layer():
         return values
 
     def __init_weights(self):
-        self.weights = np.random.random((self.input_dim, self.output_dim))
+        self.weights = np.random.random((self.output_dim, self.input_dim))
 
     def __init_biases(self):
-        self.weights = np.random.random(self.output_dim)
+        self.biases = np.random.random(self.output_dim)
+
+
+def main():
+    model = MLP(layers=[
+        Layer(2, 1, activation="linear", weights=np.array(
+            [[1, -1]]), biases=np.array([2]))
+    ])
+    values = np.array([1, 2])
+    print(model.predict(values))
+
+
+if __name__ == '__main__':
+    main()
