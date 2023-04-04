684c41e2a848d2583bf10bb1f77efede1dc4c8d2
Jasiek <jasiekk9@gmail.com>
2023-04-04 08:58:49 +0200

    Add small tweaks

--- NeuralNetworks/src/activations.py
+++ NeuralNetworks/src/activations.py
@@ -2,20 +2,34 @@ import numpy as np
 from scipy.special import expit, softmax
 
 
+def get_activation_by_name(activation):
+    if activation == "sigmoid":
+        return Sigmoid()
+    elif activation == "tanh":
+        return Tanh()
+    elif activation == "relu":
+        return ReLU()
+    elif activation == "linear":
+        return Linear()
+    elif activation == "softmax":
+        return Softmax()
+    return activation
+
+
 class Activation():
 
     __slots__ = ['activation', 'd_activation', 'last_output']
 
     def __init__(self, activation, d_activation):
         self.activation = activation
-        self.activation_prime = d_activation
+        self.d_activation = d_activation
 
     def forward(self, input):
         self.last_output = self.activation(input)
         return self.last_output
 
     def backward(self, gradient):
-        return gradient * self.activation_prime(self.last_output, activated=True)
+        return gradient * self.d_activation(self.last_output, activated=True)
 
 
 class Linear(Activation):
@@ -42,7 +56,7 @@ class Sigmoid(Activation):
     def __call__(self, values):
         return expit(values)
 
-    def derivative(self, values, activated=True, error=None):
+    def derivative(self, values, activated=True):
         # if activated set to True it's assumed that values are already an output of the sigmoid function
         if not activated:
             values = self(values)
@@ -58,7 +72,7 @@ class Tanh(Activation):
     def __call__(self, values):
         return np.tanh(values)
 
-    def derivative(self, values, activated=True, error=None):
+    def derivative(self, values, activated=True):
         # if activated set to True it's assumed that values are already an output of the tanh function
         if not activated:
             values = self(values)
@@ -77,12 +91,12 @@ class ReLU(Activation):
     def __call__(self, values):
         return np.maximum(0, values)
 
-    def derivative(self, values, activated=True, error=None):
+    def derivative(self, values, activated=True):
         # if activated set to True it's assumed that values are already an output of the relu function
         if not activated:
             values = self(values)
 
-        return (values > 0) * (1 - self.eps) + self.eps
+        return (values > 0) * (1 - self.epsilon) + self.epsilon
 
 
 class Softmax(Activation):
@@ -93,7 +107,7 @@ class Softmax(Activation):
     def __call__(self, values):
         return softmax(values, axis=0)
 
-    def derivative(self, values, activated=True, error=None):
+    def derivative(self, values, activated=True):
         # if activated set to True it's assumed that values are already an output of the softmax function
         if not activated:
             values = self(values)
