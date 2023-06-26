2a4682334cb7466b7bcf1c4e93d5beb75ce8f11e
Jasiek <jasiekk9@gmail.com>
2023-06-26 12:29:17 +0200

    Add evolutionary algorithms

---
+++ EvolutionaryAlgorithms/src/__init__.py
@@ -0,0 +1,11 @@
+from src.specimens import *
+from src.evolution import Evolution
+
+__all__ = [
+    'Evolution',
+    'Specimen',
+    'R3Quadratic',
+    'Rastring',
+    'CircleCutting',
+    'MLP',
+]
